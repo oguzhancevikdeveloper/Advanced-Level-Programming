05a4898711daa7b84f93e1298607cc283584a883
Oğuzhan Çevik <oguzhancevik.developer@outlook.com>
2024-08-24 21:14:28 +0300

    random-number-problem

---
+++ src/22_Random-Number-Problem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+Random random = new Random();
+
+for (int i = 0; i < 10; i++)
+    Console.WriteLine(random.Next()); // Her çalıştığında farklı değer
+
+Console.WriteLine("-----------------");
+
+Random random1 = new Random(100);
+
+for (int i = 0; i < 10; i++)
+    Console.WriteLine(random1.Next()); // Her çalıştırdığımızda aynı değeri üretecek. Ama farklı pc lerde farklı değer :) ortama bağlı
+
+Console.WriteLine("-----------------");
+
+Random random2 = new Random(DateTime.UtcNow.Microsecond);
+for (int i = 0; i < 10; i++)
+    Console.WriteLine(random.Next()); // Her çalıştığında farklı değer
+
